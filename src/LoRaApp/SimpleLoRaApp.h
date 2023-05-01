1115fbc2d14ca5510bd64467f44ea25616362ff4
StefanoMilani <milani@diag.uniroma1.it>
2023-05-01 13:51:45 +0200

    ADD EdgeDataFrame
    - New LoRaWAN frame EdgeDataFrame. The ED alternately a DataFrame
      and an EdgeDataFrame
    
    - GW forward to NS only EdgeDataFrame.
    
    TODO: GW: Aggragate more EdgeDataFrame and sends them together to NS
    
    Signed-Off-By: Stefano Milani <milani@diag.uniroma1.it>

--- src/LoRaApp/SimpleLoRaApp.h
+++ src/LoRaApp/SimpleLoRaApp.h
@@ -3,28 +3,28 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #ifndef __LORA_OMNET_SIMPLELORAAPP_H_
 #define __LORA_OMNET_SIMPLELORAAPP_H_
 
 #include <omnetpp.h>
-#include "inet/common/lifecycle/ILifecycle.h"
-#include "inet/common/lifecycle/NodeStatus.h"
 #include "inet/common/ModuleAccess.h"
+#include "inet/common/lifecycle/ILifecycle.h"
 #include "inet/common/lifecycle/LifecycleOperation.h"
+#include "inet/common/lifecycle/NodeStatus.h"
 
-#include "LoRaAppPacket_m.h"
 #include "LoRa/LoRaMacControlInfo_m.h"
 #include "LoRa/LoRaRadio.h"
+#include "LoRaAppPacket_m.h"
 
 using namespace omnetpp;
 using namespace inet;
@@ -34,63 +34,64 @@ namespace flora {
 /**
  * TODO - Generated class
  */
-class SimpleLoRaApp : public cSimpleModule, public ILifecycle
-{
-    protected:
-        void initialize(int stage) override;
-        void finish() override;
-        int numInitStages() const override { return NUM_INIT_STAGES; }
-        void handleMessage(cMessage *msg) override;
-        virtual bool handleOperationStage(LifecycleOperation *operation, IDoneCallback *doneCallback) override;
+class SimpleLoRaApp : public cSimpleModule, public ILifecycle {
+ protected:
+  void initialize(int stage) override;
+  void finish() override;
+  int numInitStages() const override { return NUM_INIT_STAGES; }
+  void handleMessage(cMessage* msg) override;
+  virtual bool handleOperationStage(LifecycleOperation* operation,
+                                    IDoneCallback* doneCallback) override;
 
-        void handleMessageFromLowerLayer(cMessage *msg);
-        std::pair<double,double> generateUniformCircleCoordinates(double radius, double gatewayX, double gatewayY);
-        void sendJoinRequest();
-        void sendDownMgmtPacket();
+  void handleMessageFromLowerLayer(cMessage* msg);
+  std::pair<double, double> generateUniformCircleCoordinates(double radius,
+                                                             double gatewayX,
+                                                             double gatewayY);
+  void sendJoinRequest(char* msgType);
+  void sendDownMgmtPacket();
 
-        int numberOfPacketsToSend;
-        int sentPackets;
-        int receivedADRCommands;
-        int lastSentMeasurement;
-        simtime_t timeToFirstPacket;
-        simtime_t timeToNextPacket;
+  int numberOfPacketsToSend;
+  int sentPackets;
+  int receivedADRCommands;
+  int lastSentMeasurement;
+  simtime_t timeToFirstPacket;
+  simtime_t timeToNextPacket;
 
-        cMessage *configureLoRaParameters;
-        cMessage *sendMeasurements;
+  cMessage* configureLoRaParameters;
+  cMessage* sendMeasurements;
+  cMessage* sendMeasurementsEdge;
 
-        //history of sent packets;
-        cOutVector sfVector;
-        cOutVector tpVector;
+  // history of sent packets;
+  cOutVector sfVector;
+  cOutVector tpVector;
 
-        //LoRa parameters control
-        LoRaRadio *loRaRadio;
+  // LoRa parameters control
+  LoRaRadio* loRaRadio;
 
-        void setSF(int SF);
-        int getSF();
-        void setTP(int TP);
-        double getTP();
-        void setCR(int CR);
-        int getCR();
-        void setCF(units::values::Hz CF);
-        units::values::Hz getCF();
-        void setBW(units::values::Hz BW);
-        units::values::Hz getBW();
+  void setSF(int SF);
+  int getSF();
+  void setTP(int TP);
+  double getTP();
+  void setCR(int CR);
+  int getCR();
+  void setCF(units::values::Hz CF);
+  units::values::Hz getCF();
+  void setBW(units::values::Hz BW);
+  units::values::Hz getBW();
 
+  // variables to control ADR
+  bool evaluateADRinNode;
+  int ADR_ACK_CNT = 0;
+  int ADR_ACK_LIMIT = 64;  // 64;
+  int ADR_ACK_DELAY = 32;  // 32;
+  bool sendNextPacketWithADRACKReq = false;
+  void increaseSFIfPossible();
 
-        //variables to control ADR
-        bool evaluateADRinNode;
-        int ADR_ACK_CNT = 0;
-        int ADR_ACK_LIMIT = 64; //64;
-        int ADR_ACK_DELAY = 32; //32;
-        bool sendNextPacketWithADRACKReq = false;
-        void increaseSFIfPossible();
-
-    public:
-        SimpleLoRaApp() {}
-        simsignal_t LoRa_AppPacketSent;
-
+ public:
+  SimpleLoRaApp() {}
+  simsignal_t LoRa_AppPacketSent;
 };
 
-}
+}  // namespace flora
 
 #endif
